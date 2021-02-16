58be272ccebe32cf89b419934315a259dcc4af57
Lucas Macias Atala <lucas.atala@gpcloud.com>
2021-02-16 18:05:40 -0300

    Added footer

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        <footer> Soy un footer </footer>
+  <div>
+      <!-- <b-collapse id="nav-text-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-text>Navbar text</b-nav-text>
+      </b-navbar-nav> -->
+    <!-- </b-collapse> -->
+    <div class="footer">
+    <b-navbar toggleable="lg" type="dark" variant="info" fixed="bottom">        
+        <b-nav-text>
+          Estacion de Servicio Francisco Macias - Todos los reservados derechos
+        </b-nav-text>      
+    </b-navbar>
     </div>
+  </div>
 </template>
+
+<style scoped>
+@media screen and (max-width: 1024px) {	
+	.footer {
+		display:none;
+	}
+	
+}
+</style>
--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -5,7 +5,7 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item to="/News">Noticias</b-nav-item>
+          <b-nav-item to="/News" >Noticias</b-nav-item>
           <b-nav-item to="/Contact">Contacto</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
